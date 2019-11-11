c5a8b39bb780e709478c028437f7278662f5a5d5
Sophie Clyde <mclyde@wellesley.edu>
2019-11-10 23:23:45 -0500

    partially finished prototype

---
+++ script.js
@@ -0,0 +1,28 @@
+//var savedCourses = new Array(15);
+//var savedSchedule = new Array(7);
+//var registeredCourses = new Array(6);
+
+//var checkbox = document.getElementById('materialUnchecked');
+
+//checkbox.addEventListener('materialUnchecked', function(event) {
+
+   // if(checkbox.checked){
+     //  savedCourses.push(document.getElementById("tabletext").innerHTML);
+    //}
+
+//});
+//function saveCourses() {
+   // console.log(savedCourses);
+//}
+
+//function saveSchedule() {
+    
+//}
+
+//function registerForCourses() {
+    
+//}
+
+//function dropCourse() {
+    
+//}
